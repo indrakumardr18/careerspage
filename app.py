347400765990a41b234eaff5a3e4697c861be529
indrakumardr18 <119112556+indrakumardr18@users.noreply.github.com>
2025-05-01 11:01:32 +0000

    simple application templete added

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return 'Hello from Flask!'
+  return 'Hello from indrakumar'
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5001, debug=True)
